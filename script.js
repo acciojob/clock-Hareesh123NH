433cc68e09ea9fe26246162d46bdef8f5e7fd5a1
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-17 23:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+
+function format(unit) {
+	return (unit<10)?'0'+unit:unit;
+}
+
+function upDateTime() {
+	const n=new Date();
+	// console.log(now);
+	const year=n.getFullYear();
+	const month=format(n.getMonth()+1);
+	const day=format(n.getDate());
+
+	let hours=n.getHours();
+	const minutes=format(n.getMinutes());
+	const seconds=format(n.getSeconds());
+
+	const ampm=(hours>=12)?"PM":"AM";
+	hours=hours%12;
+	hours=hours?hours:12;
+
+	const time=`${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${ampm}`;
+	
+	document.getElementById("timer").textContent=time;
+}
+setInterval(upDateTime,1000);
+upDateTime();
+
